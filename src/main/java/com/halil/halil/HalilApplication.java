c5ffa106b6ff3d03811818d701377d9885256de0
Sangmeeeee <goodman1223@naver.com>
2022-07-11 21:24:53 +0900

    first commit

---
+++ src/main/java/com/halil/halil/HalilApplication.java
@@ -0,0 +1,13 @@
+package com.halil.halil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/halil/halil/HalilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halil.halil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
