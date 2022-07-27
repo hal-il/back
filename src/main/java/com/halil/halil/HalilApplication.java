749dffc7e1fe5fc9cb7a96be0958f37fb828f9a0
Sangmeeeee <goodman1223@naver.com>
2022-07-27 22:02:39 +0900

    [feat] : swagger config 설정

--- src/main/java/com/halil/halil/HalilApplication.java
+++ src/main/java/com/halil/halil/HalilApplication.java
@@ -2,8 +2,10 @@ package com.halil.halil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class HalilApplication {
 
 	public static void main(String[] args) {
