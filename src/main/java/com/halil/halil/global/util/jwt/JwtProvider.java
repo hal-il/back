d151c77c001845315e6c7bbab4d5c654047b5acf
Sangmeeeee <goodman1223@naver.com>
2022-07-29 22:44:55 +0900

    #5 [feat] : token에서 refreshToken, accessToken으로 분리

--- src/main/java/com/halil/halil/global/util/jwt/JwtProvider.java
+++ src/main/java/com/halil/halil/global/util/jwt/JwtProvider.java
@@ -14,16 +14,28 @@ public class JwtProvider {
     @Value("${jwt.secret}")
     private String SECRET_KEY;
 
-    private Long expiredTime = 1000 * 60L;
+    private Long accessTokenExpiredTime = 1000 * 60L;
 
-    public String getToken(String nickName, String email){
+    private Long refreshTokenExpiredTime = 1000 * 60L * 24 * 14;
+
+    public String getAccessToken(String nickName, String email){
         Date now = new Date();
         return Jwts.builder().setHeaderParam(Header.TYPE, Header.JWT_TYPE)
                 .setIssuer("halil")
                 .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + expiredTime))
+                .setExpiration(new Date(now.getTime() + accessTokenExpiredTime))
                 .claim("nickName", nickName)
                 .claim("email", email)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY.getBytes())
+                .compact();
+    }
+
+    public String getRefreshToken(){
+        Date now = new Date();
+        return Jwts.builder().setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuer("halil")
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + refreshTokenExpiredTime))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY.getBytes())
                 .compact();
     }
