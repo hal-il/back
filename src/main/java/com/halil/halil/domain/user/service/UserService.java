956a0e0eedda83dca917a4a02520bef01913e3c5
yein <syi05111@naver.com>
2022-07-29 14:20:39 +0900

    #7 [feat] : UserService 수정

--- src/main/java/com/halil/halil/domain/user/service/UserService.java
+++ src/main/java/com/halil/halil/domain/user/service/UserService.java
@@ -1,12 +1,12 @@
 package com.halil.halil.domain.user.service;
 
+import com.halil.halil.domain.user.dto.UserResponseDto;
 import com.halil.halil.domain.user.dto.UserUpdateRequestDto;
 import com.halil.halil.domain.user.entity.User;
 
 import java.util.Optional;
 
 public interface UserService {
-    User updateUserInfo(UserUpdateRequestDto userUpdateRequestDto);
-    User findByEmail(String email);
+    UserResponseDto updateUserInfo(UserUpdateRequestDto userUpdateRequestDto);
 
 }
