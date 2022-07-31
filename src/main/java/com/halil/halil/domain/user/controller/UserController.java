4e900c94893879ff4408a15a2756af23e3c5c1b3
yein <syi05111@naver.com>
2022-08-01 02:54:10 +0900

    #7 [refactor] : UserController 로직 수정

--- src/main/java/com/halil/halil/domain/user/controller/UserController.java
+++ src/main/java/com/halil/halil/domain/user/controller/UserController.java
@@ -1,17 +1,17 @@
 package com.halil.halil.domain.user.controller;
 
+import com.halil.halil.domain.user.dto.UserResponseDto;
 import com.halil.halil.domain.user.dto.UserUpdateRequestDto;
-import com.halil.halil.domain.user.exception.ExistNicknameException;
 import com.halil.halil.domain.user.service.UserService;
 import com.halil.halil.global.common.CommonResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
@@ -22,11 +22,9 @@ public class UserController {
     private final UserService userService;
 
     @PutMapping()
-    public ResponseEntity<?> updateUserInfo(@RequestBody @Valid UserUpdateRequestDto userUpdateRequestDto){
-        try{
-            return ResponseEntity.ok(CommonResponse.createSuccess(userService.updateUserInfo(userUpdateRequestDto)));
-        }catch (DataIntegrityViolationException e){
-            throw new ExistNicknameException("이미 존재하는 닉네임입니다.");
-        }
+    public ResponseEntity<?> updateUserInfo(HttpServletRequest request, @RequestBody @Valid UserUpdateRequestDto userUpdateRequestDto){
+        String email = (String)request.getAttribute("email");
+        UserResponseDto userResponseDto = userService.updateUserInfo(email,userUpdateRequestDto);
+        return ResponseEntity.ok(CommonResponse.createSuccess(userResponseDto));
     }
 }
