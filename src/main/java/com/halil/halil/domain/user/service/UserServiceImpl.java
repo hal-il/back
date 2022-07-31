addb61e58e84ce382e7391faa894946a0f4ae28d
yein <syi05111@naver.com>
2022-08-01 02:56:24 +0900

    #7 [refactor] : updateRefreshToken 메서드 추가, 로직 구현

--- src/main/java/com/halil/halil/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/halil/halil/domain/user/service/UserServiceImpl.java
@@ -4,7 +4,10 @@ import com.halil.halil.domain.user.dto.UserResponseDto;
 import com.halil.halil.domain.user.dto.UserUpdateRequestDto;
 import com.halil.halil.domain.user.entity.User;
 import com.halil.halil.domain.user.repository.UserRepository;
+import com.halil.halil.global.exception.ExistNicknameException;
+import com.halil.halil.global.exception.NoExistUserException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
@@ -16,17 +19,24 @@ public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
 
     @Override
-    public UserResponseDto updateUserInfo(UserUpdateRequestDto userUpdateRequestDto) {
-        String email = userUpdateRequestDto.getEmail();
-        String nickname = userUpdateRequestDto.getNickname();
-
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new NoSuchElementException());
-        user.update(nickname);
-
-        userRepository.save(user);
-
-        UserResponseDto userResponseDto = new UserResponseDto(user);
-        return userResponseDto;
+    public UserResponseDto updateUserInfo(String email, UserUpdateRequestDto userUpdateRequestDto) {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new NoExistUserException("존재하는 회원정보가 없습니다."));
+        try{
+            user.update(userUpdateRequestDto.getNickname());
+            userRepository.save(user);
+            UserResponseDto userResponseDto = new UserResponseDto(user);
+            return userResponseDto;
+        }catch (DataIntegrityViolationException e){
+            throw new ExistNicknameException("존재하는 닉네임입니다.");
+        }
     }
+
+    @Override
+    public void updateRefreshToken(String email, String refreshToken) {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new NoExistUserException("존재하는 회원정보가 없습니다."));
+        user.updateRefreshToken(refreshToken);
+        userRepository.save(user);
+    }
+
 
 }
