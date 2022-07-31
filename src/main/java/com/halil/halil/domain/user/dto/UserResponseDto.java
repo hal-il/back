0b6a0b69ba5e441e996051a62f29a752a4981a6e
yein <syi05111@naver.com>
2022-08-01 02:54:48 +0900

    #7 [refactor] : UserResponseDto에 JsonInclude 어노테이션 추가

--- src/main/java/com/halil/halil/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/halil/halil/domain/user/dto/UserResponseDto.java
@@ -1,9 +1,11 @@
 package com.halil.halil.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.halil.halil.domain.user.entity.User;
 import lombok.Getter;
 
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponseDto {
 
     private String email;
