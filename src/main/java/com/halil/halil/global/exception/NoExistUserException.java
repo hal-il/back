b03ce1f81ee98d9b8d94221b407e7c5b3c756a44
yein <77979505+yeinshin@users.noreply.github.com>
2022-08-01 23:05:56 +0900

    Hi 4/feature/user/update 사용자는 자신의 회원 정보를 수정할 수 있다. (#11)
    * #4 [feat] : User Entity 생성
    
    * Squashed commit of the following:
    
    commit 749dffc7e1fe5fc9cb7a96be0958f37fb828f9a0
    Author: Sangmeeeee <goodman1223@naver.com>
    Date:   Wed Jul 27 22:02:39 2022 +0900
    
        [feat] : swagger config 설정
    
    commit 02348156af3e901d05e808d3c3d408de69da7f78
    Author: Sangmeeeee <goodman1223@naver.com>
    Date:   Wed Jul 27 22:02:02 2022 +0900
    
        [build] : swagger 의존성 추가
    
    commit 69d2342b1177cd39c65944393906a0924103e21e
    Author: Sangmeeeee <goodman1223@naver.com>
    Date:   Wed Jul 27 20:41:58 2022 +0900
    
        [build] spring-boot-validation 의존성 추가
    
    commit a2b45526a4f504e2ebf9e324fbaa22ce35a3b010
    Author: Sangmeeeee <goodman1223@naver.com>
    Date:   Mon Jul 25 04:08:53 2022 +0900
    
        [build] : jwt 의존성 추가
    
    commit d057f0ba438410257ba4cca2ddb8f88143b1e068
    Author: Sangmeeeee <goodman1223@naver.com>
    Date:   Tue Jul 19 22:35:25 2022 +0900
    
        [chore] : application.properties 삭제
    
    commit 4fd1818b220f4a3fc6d8a9a91e4f10fc211bf932
    Author: Sangmeeeee <goodman1223@naver.com>
    Date:   Tue Jul 19 22:26:12 2022 +0900
    
        [chore] : .gitignore 변경
    
    commit 6d1fe4bc0a86a65f57bec4225c8d345091ae9f49
    Author: Sangmeeeee <goodman1223@naver.com>
    Date:   Tue Jul 19 21:27:29 2022 +0900
    
        Update issue templates
    
    * #4 [feat] : User Entity Package 변경
    
    * #4 [feat] : UserRepository 생성, UserRepositoryTest 생성
    
    * #4 [feat] : UserService 생성, UserServiceImpl 생성
    
    * #4 [feat] : User Entity에 update 메소드 추가
    
    * #4 [feat] : UserController 생성
    
    * #4 [feat] : UserUpdateRequestDto 생성
    
    * #4 [feat] : CommonResponse 공통 Response 객체 생성
    
    * #4 [feat] : ExistNicknameException 생성
    
    * #7 [feat] : UserResponseDto 생성
    
    * #7 [feat] : UserService 수정
    
    * #7 [feat] : UserServiceImpl 수정
    
    * #7 [feat] : UserUpdateRequestDto에 validation annotation 적용
    
    * #7 [feat] : ExistNicknameException 클래스 추가
    
    * #7 [feat] : JwtInterceptor 인터셉터 추가
    
    * #7 [feat] : JwtService 클래스 추가
    
    * #7 [feat] : JwtServiceImpl 클래스 추가
    
    * #7 [feat] : NoExistUserException 예외 클래스 추가
    
    * #7 [refactor] : User Entity에 refreshToken 필드, updateRefreshToken 추가
    
    * #7 [refactor] : UserController 로직 수정
    
    * #7 [refactor] : UserResponseDto에 JsonInclude 어노테이션 추가
    
    * #7 [refactor] : updateRefreshToken 메서드 추가
    
    * #7 [refactor] : updateRefreshToken 메서드 추가, 로직 구현
    
    * #7 [feat] : WebMvcConfiguration 추가
    
    Co-authored-by: Sangmeeeee <goodman1223@naver.com>

---
+++ src/main/java/com/halil/halil/global/exception/NoExistUserException.java
@@ -0,0 +1,7 @@
+package com.halil.halil.global.exception;
+
+public class NoExistUserException extends RuntimeException{
+    public NoExistUserException(String message){
+        super(message);
+    }
+}
