cac624c9af87fd03864a6e817d7485e232b59eec
Sangmeeeee <goodman1223@naver.com>
2022-08-03 01:27:19 +0900

    #15 [refactor] : jwt에서 accessToken, refreshToken으로 변경

--- src/main/java/com/halil/halil/domain/user/dto/UserLoginResponseDto.java
+++ src/main/java/com/halil/halil/domain/user/dto/UserLoginResponseDto.java
@@ -6,5 +6,6 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class UserLoginResponseDto {
-    private String jwt;
+    private String accessToken;
+    private String refreshToken;
 }
