50b130de924bf6660c9a9dc8d76aad218b232de7
twknds <twknds@naver.com>
2022-08-03 10:24:20 +0900

    #13 [refactor] : advice미추가로 다시 추가

---
+++ src/main/java/com/halil/halil/domain/user/advice/UserControllerAdvice.java
@@ -0,0 +1,22 @@
+package com.halil.halil.domain.user.advice;
+
+import com.halil.halil.global.response.CommonResponse;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class UserControllerAdvice {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CommonResponse> invalidDtoException(MethodArgumentNotValidException e){
+        return new ResponseEntity<>(CommonResponse.createError("take a form plz"), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<CommonResponse> ExistUserNickNameException(DataIntegrityViolationException e){
+        return new ResponseEntity<>(CommonResponse.createError("Already Exist NickName"), HttpStatus.BAD_REQUEST);
+    }
+
+}
