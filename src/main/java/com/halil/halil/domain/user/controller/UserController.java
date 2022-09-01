924d9ed0d9163c0ed7d30d4f3016dc5e39161bb3
Sangmeeeee <goodman1223@naver.com>
2022-09-01 22:46:36 +0900

    #15 [refactor] : @Validated 제거

--- src/main/java/com/halil/halil/domain/user/controller/UserController.java
+++ src/main/java/com/halil/halil/domain/user/controller/UserController.java
@@ -24,7 +24,6 @@ public class UserController {
     private final UserServiceImpl userService;
 
     @ApiOperation(value = "", notes = "해당 api로 요청시 google 로그인 화면으로 리다이렉트 해줌")
-    @Validated
     @GetMapping
     ResponseEntity<CommonResponse> getUserByCode(@ApiParam(name = "code", type = "String", value = "구글 코드", example = "your code") @NotBlank @RequestParam("code") String code){
         return new ResponseEntity<>(CommonResponse.createSuccess(userService.findUserByCode(code)), HttpStatus.OK);
