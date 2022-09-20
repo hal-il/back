df3b2411aba214a388b0cd2f3650ad38eefb16a1
twknds <twknds@naver.com>
2022-09-20 16:13:49 +0900

    [feature] Advice위치 변경 및 빈객체 삭제시 exception추가

--- src/main/java/com/halil/halil/global/advice/UserControllerAdvice.java
+++ src/main/java/com/halil/halil/global/advice/UserControllerAdvice.java
@@ -1,7 +1,8 @@
-package com.halil.halil.domain.user.advice;
+package com.halil.halil.global.advice;
 
 import com.halil.halil.global.response.CommonResponse;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,13 +11,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class UserControllerAdvice {
-    /*@ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<CommonResponse> invalidDtoException(MethodArgumentNotValidException e){
-        return new ResponseEntity<>(CommonResponse.createError("take a form plz"), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(CommonResponse.createError("take a Dto form plz"), HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<CommonResponse> ExistUserNickNameException(DataIntegrityViolationException e){
-        return new ResponseEntity<>(CommonResponse.createError("Already Exist NickName"), HttpStatus.BAD_REQUEST);
-    }*/
-
+        return new ResponseEntity<>(CommonResponse.createError("Plz Check Dto Data"), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public  ResponseEntity<CommonResponse> EmptyDataDeleteException(EmptyResultDataAccessException e){
+        return new ResponseEntity<>(CommonResponse.createError("Dont Delete Empty Entity"), HttpStatus.BAD_REQUEST);
+    }
 }
