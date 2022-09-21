dc81214a6da951f0a52f8a4e1fd31aedc205a0d7
Sangmeeeee <goodman1223@naver.com>
2022-09-21 15:54:55 +0900

    #15 [test] : UserServiceTest refactoring

--- src/test/java/com/halil/halil/domain/user/serivce/UserServiceTest.java
+++ src/test/java/com/halil/halil/domain/user/serivce/UserServiceTest.java
@@ -1,48 +1,71 @@
 package com.halil.halil.domain.user.serivce;
 
+import com.halil.halil.domain.login.service.LoginService;
 import com.halil.halil.domain.user.dto.UserCreateRequestDto;
+import com.halil.halil.domain.user.dto.UserCreateResponseDto;
 import com.halil.halil.domain.user.entity.User;
-import com.halil.halil.domain.user.exception.ExistUserException;
+import com.halil.halil.domain.user.exception.ExistNicknameException;
 import com.halil.halil.domain.user.repository.UserRepository;
-import com.halil.halil.domain.user.service.UserCreateServiceImpl;
-import com.halil.halil.domain.user.service.UserService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataIntegrityViolationException;
+import com.halil.halil.domain.user.service.UserServiceImpl;
+import com.halil.halil.global.util.jwt.JwtProvider;
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.Optional;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 
-@SpringBootTest
 class UserServiceTest {
-    @Autowired
-    private UserCreateServiceImpl userCreateServiceImpl;
-    @Autowired
-    private UserRepository userRepository;
 
-    private UserCreateRequestDto userCreateRequestDto1 = new UserCreateRequestDto("email1","nickname1");
-    private UserCreateRequestDto userCreateRequestDto2 = new UserCreateRequestDto("email2","nickname2");
-    private UserCreateRequestDto userCreateRequestDto3 = new UserCreateRequestDto("email3","nickname3");
-    void Setup(){
+    private UserServiceImpl userService;
 
+    private UserRepository userRepository = Mockito.mock(UserRepository.class);
+    private JwtProvider jwtProvider = Mockito.mock(JwtProvider.class);
+    private LoginService loginService = Mockito.mock(LoginService.class);
+
+    @BeforeEach
+    void init(){
+        userService = new UserServiceImpl(userRepository, jwtProvider, loginService);
     }
+
     @Test
-    @DisplayName("Already Exist User Exception Test")
-    void AlreadyExistUserTest(){
-        UserCreateRequestDto dupUserCreateRequestDto = new UserCreateRequestDto("email1","nickname1");
-        userCreateServiceImpl.CreateUser(userCreateRequestDto1);
-        Assertions.assertThrows(DataIntegrityViolationException.class, () -> userCreateServiceImpl.CreateUser(dupUserCreateRequestDto));
+    @DisplayName("중복 회원 가입")
+    void duplicatedSignUpTest(){
+        String duplicatedNickName = "duplicatedNickName";
+        String duplicatedEmail = "duplicatedEmail";
+        UserCreateRequestDto userCreateRequestDto = new UserCreateRequestDto();
+        userCreateRequestDto.setEmail(duplicatedEmail);
+        userCreateRequestDto.setNickname(duplicatedNickName);
+
+        Mockito.when(userRepository.save(any(User.class))).thenThrow(new ExistNicknameException());
+
+        Assertions.assertThrows(ExistNicknameException.class, () -> userService.createUser(userCreateRequestDto));
+        verify(userRepository).save(any(User.class));
     }
+
     @Test
-    @DisplayName("No nickName in User Test")
-    void NotExistUserNameTest(){
-        UserCreateRequestDto userCreateRequestDto = new UserCreateRequestDto("nickname",null);
-        Assertions.assertThrows(DataIntegrityViolationException.class, () -> userCreateServiceImpl.CreateUser(userCreateRequestDto));
+    @DisplayName("회원 가입 성공")
+    void successSignUpTest(){
+        String nickname = "nickName";
+        String email = "email";
+        String accessToken = "accessToken";
+        String refreshToken = "refreshToken";
+        UserCreateRequestDto userCreateRequestDto = new UserCreateRequestDto();
+        userCreateRequestDto.setNickname(nickname);
+        userCreateRequestDto.setEmail(email);
+
+        UserCreateResponseDto userCreateResponseDto = new UserCreateResponseDto(accessToken, refreshToken);
+
+        Mockito.when(jwtProvider.getAccessToken(email)).thenReturn("accessToken");
+        Mockito.when(jwtProvider.getRefreshToken()).thenReturn("refreshToken");
+
+        Assertions.assertEquals(userCreateResponseDto.getAccessToken(), userService.createUser(userCreateRequestDto).getAccessToken());
+        Assertions.assertEquals(userCreateResponseDto.getRefreshToken(), userService.createUser(userCreateRequestDto).getRefreshToken());
+    }
+
+    @Test
+    @DisplayName("닉네임 조회 실패")
+    void notExistUserNickNameTest(){
+
     }
 }
