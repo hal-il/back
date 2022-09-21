86fb68d5f98ae5b34ad5b00b0544135186acd5b1
Sangmeeeee <goodman1223@naver.com>
2022-09-21 15:55:18 +0900

    #15 [refactor] : UserService refactoring

--- src/main/java/com/halil/halil/domain/user/service/UserService.java
+++ src/main/java/com/halil/halil/domain/user/service/UserService.java
@@ -5,9 +5,6 @@ import com.halil.halil.domain.user.dto.UserResponseDto;
 import com.halil.halil.domain.user.dto.UserUpdateRequestDto;
 import com.halil.halil.domain.user.dto.UserCreateRequestDto;
 import com.halil.halil.domain.user.dto.UserCreateResponseDto;
-import com.halil.halil.domain.user.entity.User;
-
-import java.util.Optional;
 
 public interface UserService {
     UserLoginResponseDto findUserByCode(String code);
